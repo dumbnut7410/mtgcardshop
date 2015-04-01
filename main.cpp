8fa828ed195590df8820b8ac084a0ec7b00b9723
dumbnut7410 <gilderjw@rose-hulman.edu>
2015-04-01 10:17:20 -0400

    tested, everything appears to work, added an s to transactions so help is not misleading

--- main.cpp
+++ main.cpp
@@ -234,7 +234,7 @@ int main(int argc, char *argv[])
                       << "sell \n"
                       << "addplayer \n"
                       << "pnl \n"
-                      << "transaction \n"
+                      << "transactions \n"
                       << "playerlookup"
                       << std::endl;
             break;
